f4a472b068ffffb98952cf62b7fd22f9299b92a8
David Pietrzak <dtpietrzak@gmail.com>
2024-08-16 08:01:43 -0500

    Update app.tsx

---
+++ src/apps/TextEditor/app.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { useState } from "react"
+
+import { ScrollBar } from "@/components/ScrollBar"
+import { App, OpenWindow, renderWindow } from "@/os/windows"
+// import { Application } from "@/os/apps"
+
+// export const App: Application = {
+//   title: 'File Explorer',
+//   multiInstance: false,
+//   app: (osData) => <FileExplorer />,
+// }
+
+export const openTextEditor: OpenWindow = ({
+  forApp, forRender,
+}) => {
+  return renderWindow({
+    title: 'Text Editor',
+    windowId: 'text-editor',
+    app: <TextEditor {...forApp} />,
+  }, forRender)
+}
+
+const TextEditor: App = (osData) => {
+  const [text, setText] = useState<string>("")
+
+  return (
+    <ScrollBar>
+      <textarea
+        className={'w-full h-full'}
+        value={text}
+        onChange={(e) => {
+          setText(e.currentTarget.value)
+        }}
+      />
+    </ScrollBar>
+  )
+}
