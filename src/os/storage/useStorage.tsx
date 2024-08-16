3a903dd770bf73fce6275da267fd8e835c506f6e
dtpietrzak <dtpietrzak@gmail.com>
2024-08-15 22:22:33 -0500

    commented out temp storage code

--- src/os/storage/useStorage.tsx
+++ src/os/storage/useStorage.tsx
@@ -22,32 +22,32 @@ export const StorageProvider = ({
   )
 }
 
-export const useStorage = () => {
+// export const useStorage = () => {
 
-  const toSave = (data: any) => {
-    return JSON.stringify(data)
-  }
+//   const toSave = (data: any) => {
+//     return JSON.stringify(data)
+//   }
 
-  const saveSystem = (system: System) => {
-    try {
-      // localStorage
+//   const saveSystem = (system: System) => {
+//     try {
+//       // localStorage
 
-      const saveId = system.user.userId + "-system"
-      const saveData = toSave(system)
+//       const saveId = system.user.userId + "-system"
+//       const saveData = toSave(system)
 
-      localStorage.setItem(saveId, saveData)
+//       localStorage.setItem(saveId, saveData)
 
-    } catch (error) {
-      console.error("Failed to save to localStorage", error)
-    }
+//     } catch (error) {
+//       console.error("Failed to save to localStorage", error)
+//     }
 
-    try {
-      // TODO: server
-    } catch (error) {
-      console.error("Failed to save to server", error)
-    }
-  }
-}
+//     try {
+//       // TODO: server
+//     } catch (error) {
+//       console.error("Failed to save to server", error)
+//     }
+//   }
+// }
 
 
 
