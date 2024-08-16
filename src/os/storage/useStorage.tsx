f3957f588496495f591fd24865da8bab8af18383
David Pietrzak <dtpietrzak@gmail.com>
2024-08-15 20:06:02 -0500

    Update useStorage.tsx

--- src/os/storage/useStorage.tsx
+++ src/os/storage/useStorage.tsx
@@ -22,4 +22,45 @@ export const StorageProvider = ({
   )
 }
 
-export const useStorage = () => useContext(StorageContext)
+export const useStorage = () => {
+
+  const toSave = (data: any) => {
+    return JSON.stringify(data)
+  }
+
+  const saveSystem = (system: System) => {
+    try {
+      // localStorage
+
+      const saveId = system.user.userId + "-system"
+      const saveData = toSave(system)
+
+      localStorage.setItem(saveId, saveData)
+
+    } catch (error) {
+      console.error("Failed to save to localStorage", error)
+    }
+
+    try {
+      // TODO: server
+    } catch (error) {
+      console.error("Failed to save to server", error)
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
